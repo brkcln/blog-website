9978d29527f388cc9a40ac349696b28cc00d139b
Burak <burakcelen17@gmail.com>
2022-03-31 00:32:36 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-
+var swiper = new Swiper(".mySwiper", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2300,
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
